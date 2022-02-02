0204e9d1a8def6197aaa4b249ffea7cc4d8dd5ef
Jo Faria <joana.adaodefaria@gmail.com>
2022-02-02 07:47:12 +0000

    solved lab

---
+++ routes/celebrities.routes.js
@@ -0,0 +1,38 @@
+// routes/celebrities.routes.js
+
+const router = require("express").Router();
+const Celebrity = require("../models/Celebrity.model");
+
+// all your routes here
+
+// ***** CREATE ROUTES *****
+
+// GET '/celebrities/create' route to show celebrity creation form to the user
+router.get("/create", (req, res, next) => {
+  res.render("celebrities/new-celebrity.hbs"); // in res.render() we never start with '/'. Physical path to hbs file
+});
+
+// POST '/celebrities/create' route to create a new celebrity in the DB
+router.post("/create", (req, res, next) => {
+  const { name, occupation, catchPhrase } = req.body;
+
+  Celebrity.create({ name, occupation, catchPhrase })
+    .then(() => res.redirect("/celebrities"))
+    .catch((err) => {
+      console.log(err);
+      res.render("celebrities/new-celebrity.hbs");
+    });
+});
+
+// ***** READ ROUTES *****
+
+// GET '/celebrities' to show all celebrities in a list
+router.get("/", (req, res, next) => {
+  Celebrity.find()
+    .then((allCelebrities) =>
+      res.render("celebrities/celebrities.hbs", { allCelebrities })
+    )
+    .catch((err) => next(err));
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
+// routes/index.js
+
 const router = require("express").Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+/* Celebrity Routes. All routes will start as '/celebrities/...' */
+const celebrityRoutes = require("./celebrities.routes");
+router.use("/celebrities", celebrityRoutes);
+
+/* Movie Routes. All routes will start as '/movies/...' */
+const movieRoutes = require("./movies.routes");
+router.use("/movies", movieRoutes);
 
 module.exports = router;
